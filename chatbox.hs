f90e5eaf7ed72b094174d027352a718c746d17ea
Dave Compton <davecompton7@gmail.com>
2015-04-12 21:42:28 -0700

    introducing sniffIO, processIO, and sendIO which do in the IO monad what sniff, process, and send did in the Server monad.  These allow the use of forkIO which appears to work - unlike forkServerIO

--- chatbox.hs
+++ chatbox.hs
@@ -72,7 +72,6 @@ process state hndl = do
         _ -> return ()
     process state hndl -- loop forever
 
-
 sniff :: Server State -> Server ()
 sniff state = do
   let hndl = openLive "wlan0" 500 False 0
@@ -81,23 +80,29 @@ sniff state = do
   liftIO $ setFilter handle "tcp[13] & 7!=0" True 0
   process state hndl'
 
-processIO :: PcapHandle -> IO ()
-processIO hndl = do
+-- | Send a message; keep a backlog of 100 messages.
+sendIO :: IO State -> String -> String -> IO ()
+sendIO state sender msg = do
+  (clients, messages) <- state
+  cs <- readIORef clients
+  atomicModifyIORef messages $ \msgs -> ((sender, msg):take 99 msgs, ())
+  -- Fork a new thread for each MVar so slow clients don't hold up fast ones.
+  forM_ cs $ \(_, v) -> C.forkIO $ C.putMVar v (sender, msg)
+
+processIO :: IO State -> PcapHandle -> IO ()
+processIO state hndl = do
     (hdr,pkt) <- Network.Pcap.next hndl
     bytes <- peekArray (fromIntegral (hdrCaptureLength hdr)) pkt
     case filterEthernet bytes of 
-        -- Just packet -> send state "sniff" $ show packet
-        Just packet -> putStrLn $ show packet
-        -- _ -> print bytes
+        Just packet -> sendIO state "sniff" $ show packet
+        -- Just packet -> putStrLn $ show packet
         _ -> return ()
-    processIO hndl -- loop forever
+    processIO state hndl -- loop forever
 
-
-sniffIO :: IO ()
-sniffIO = do
+sniffIO :: IO State -> IO ()
+sniffIO state = do
   handle <- openLive "wlan0" 500 False 0
-  processIO handle 
-
+  processIO state handle 
 
 -- | Scroll to the bottom of a textarea.
 scrollToBottom :: Elem -> Client ()
@@ -141,11 +146,11 @@ main = do
     state <- liftServerIO $ do
       clients <- newIORef []
       messages <- newIORef []
-      return (clients, messages)
+      let stateIO = return (clients,messages)
+      C.forkIO $ sniffIO stateIO
+      stateIO
 
-    forkServerIO $ sniff state
-    -- liftServerIO $ sniffIO
-
+    -- forkServerIO $ sniff state
 
     -- Create an API object holding all available functions
     api <- API <$> remote (hello state)
