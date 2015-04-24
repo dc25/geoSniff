ac96a0947eeae960167e9e24c4b2197fdf6c0852
Dave Compton <davecompton7@gmail.com>
2015-04-24 09:24:08 -0700

    cleaned up indentation

--- mapConnections.hs
+++ mapConnections.hs
@@ -24,12 +24,12 @@ data Message = Message Packet Double Double deriving Show
 
 -- Message must be instance of Binary for server -> client communication.
 instance Binary Message where
-  put (Message pkt long lat) = put pkt >> put long >> put lat
-  get = do
-      pk <- get
-      lo <- get
-      la <- get
-      return $ Message pk lo la
+    put (Message pkt long lat) = put pkt >> put long >> put lat
+    get = do
+        pk <- get
+        lo <- get
+        la <- get
+        return $ Message pk lo la
 
 nullMessage :: Message
 nullMessage = Message nullPacket 0.0 0.0
@@ -42,35 +42,35 @@ type State = IORef [(SessionID, C.MVar Message)]
 data API = API {
     apiHello :: Remote (Server ()),
     apiAwait :: Remote (Server Message)
-  }
+}
 
 -- | Tell the server we're here and remove any stale sessions.
 hello :: Server State -> Server ()
 hello state = do
-  sid <- getSessionID
-  active <- getActiveSessions
-  clients <- state
-  liftIO $ do
-    v <- C.newEmptyMVar
-    atomicModifyIORef clients $ \cs ->
-      ((sid, v) : filter (\(sess, _) -> sess `S.member` active) cs, ())
-    return ()
+    sid <- getSessionID
+    active <- getActiveSessions
+    clients <- state
+    liftIO $ do
+        v <- C.newEmptyMVar
+        atomicModifyIORef clients $ \cs ->
+          ((sid, v) : filter (\(sess, _) -> sess `S.member` active) cs, ())
+        return ()
 
 -- | Send a message by notifying everyone who is waiting for a message.
 send :: Server State -> Message -> Server ()
 send state msg = do
-  clients <- state
-  liftIO $ do
-    cs <- readIORef clients
-    -- Fork a new thread for each MVar so slow clients don't hold up fast ones.
-    forM_ cs $ \(_, v) -> C.forkIO $ C.putMVar v msg
+    clients <- state
+    liftIO $ do
+        cs <- readIORef clients
+        -- Fork a new thread for each MVar so slow clients don't hold up fast ones.
+        forM_ cs $ \(_, v) -> C.forkIO $ C.putMVar v msg
 
 -- | Block until a new message arrives, then return it.
 await :: Server State -> Server Message
 await state = do
-  sid <- getSessionID
-  clients <- state
-  liftIO $ readIORef clients >>= maybe (return nullMessage) C.takeMVar . lookup sid
+    sid <- getSessionID
+    clients <- state
+    liftIO $ readIORef clients >>= maybe (return nullMessage) C.takeMVar . lookup sid
 
 #ifndef __HASTE__
 
@@ -109,10 +109,10 @@ process state handle localIPv4 liveConnections getIPLocation' = do
 
 sniff :: Server State -> Server ()
 sniff state = do
-  handle <- liftIO $ openLive "wlan0" 500 False 0
-  localInterfaces <- liftIO N.getNetworkInterfaces
-  let localIPv4 = fmap N.ipv4 localInterfaces
-  process state handle localIPv4 S.empty getIPLocation 
+    handle <- liftIO $ openLive "wlan0" 500 False 0
+    localInterfaces <- liftIO N.getNetworkInterfaces
+    let localIPv4 = fmap N.ipv4 localInterfaces
+    process state handle localIPv4 S.empty getIPLocation 
 #else
 sniff :: Server State -> Server ()
 sniff state = return ()
@@ -131,27 +131,27 @@ awaitLoop api chatlines = do
 -- | Client entry point.
 clientMain :: API -> Client ()
 clientMain api = do
-  -- Tell the server we're here.
-  onServer (apiHello api)
+    -- Tell the server we're here.
+    onServer (apiHello api)
 
-  -- Ask the server for a new message, block until one arrives, repeat
-  fork $ awaitLoop api []
+    -- Ask the server for a new message, block until one arrives, repeat
+    fork $ awaitLoop api []
 
 
 -- | Launch the application!
 main :: IO ()
 main = 
-  -- Run the Haste.App application. Please note that a computation in the App
-  -- monad should never contain any free variables.
-  runApp (mkConfig "ws://192.168.1.194:24601" 24601) $ do
-    -- Create our state-holding elements
-    state <- liftServerIO $ newIORef []
+    -- Run the Haste.App application. Please note that a computation in the App
+    -- monad should never contain any free variables.
+    runApp (mkConfig "ws://192.168.1.194:24601" 24601) $ do
+        -- Create our state-holding elements
+        state <- liftServerIO $ newIORef []
 
-    forkServerIO $ sniff state
+        forkServerIO $ sniff state
 
-    -- Create an API object holding all available functions
-    api <- API <$> remote (hello state)
-               <*> remote (await state)
+        -- Create an API object holding all available functions
+        api <- API <$> remote (hello state)
+                   <*> remote (await state)
 
-    -- Launch the client
-    runClient $ clientMain api
+        -- Launch the client
+        runClient $ clientMain api
