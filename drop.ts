a521e88ed15aaed364f767a10432b57115533bf8
Dave Compton <davecompton7@gmail.com>
2015-05-09 13:04:35 -0700

    choosing a better center again.

--- drop.ts
+++ drop.ts
@@ -6,7 +6,7 @@ var map;
 function initialize() {
   var mapOptions = {
     zoom: 2,
-    center: new google.maps.LatLng(20.0, 13.410186)
+    center: new google.maps.LatLng(20.0, 0.0)
   };
 
   map = new google.maps.Map(document.getElementById('map-canvas'),
